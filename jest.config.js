d1d449933552ead93c22205fcafc73636180b0cc
José Victor <josevictordev@gmail.com>
2023-06-04 16:43:54 -0300

    chore: config jest testes and test to render login screen

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/mocks/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^images/(.*)$': '<rootDir>/src/assets/images/$1',
+    '^containers/(.*)$': '<rootDir>/src/containers/$1',
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+  },
+};
