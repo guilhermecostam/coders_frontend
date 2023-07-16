1da8ead0e41417ec7e96e7cb8924f49612a68f47
victormedeiros1 <josevictordev@gmail.com>
2023-07-16 19:37:39 -0300

    refactor: logic to send request

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,45 +1,46 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { LoadingButton } from '@mui/lab';
-import {
-  Grid, TextField, Typography,
-} from '@mui/material';
+import { Grid, TextField, Typography } from '@mui/material';
 import Logo from 'components/Logo';
 import Container from 'containers/Container';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import Toast from 'src/components/Toast';
 import { setSessionToken } from 'src/helpers/session';
+import { authenticationUserFormSchema } from 'src/helpers/validations';
 import { signIn } from 'src/services/requests';
 
 export default function Login() {
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [message, setMessage] = useState('');
+  const [open, setOpen] = useState(false);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: zodResolver(authenticationUserFormSchema),
+  });
+
+  const authenticationUser = async (data) => {
     setLoading(true);
 
-    const userData = {
-      identifier: event.target.elements.identifier.value,
-      password: event.target.elements.password.value,
-    };
-
-    const response = await signIn(userData);
+    const response = await signIn(data);
 
     if (response.status === 200) {
       setLoading(false);
       setSessionToken(response.data.token);
-    } else {
-      setLoading(false);
-      setError(true);
-
-      setTimeout(() => {
-        setError(false);
-      }, 5000);
+      navigate('/feed');
     }
+
+    setLoading(false);
+    setMessage('Usuário ou senha incorretos.');
+    setOpen(true);
   };
 
   return (
     <Container>
-      <form onSubmit={handleSubmit}>
+      <Toast message={message} severity="error" open={open} setOpen={setOpen} />
+      <form onSubmit={handleSubmit(authenticationUser)}>
         <Grid container flexDirection="column" textAlign="center" spacing={2} maxWidth={300}>
           <Grid item>
             <Logo />
@@ -48,10 +49,10 @@ export default function Login() {
             <Typography variant="h6" color="text.secondary">EI CODER, LOGUE AGORA!</Typography>
           </Grid>
           <Grid item>
-            <TextField name="identifier" label="Email" type="text" size="small" error={error} helperText={error && 'Usuário inválido'} fullWidth />
+            <TextField {...register('identifier')} label="Usuário" type="text" size="small" error={!!errors.identifier} helperText={errors.identifier && errors.identifier.message} fullWidth />
           </Grid>
           <Grid item>
-            <TextField name="password" label="Senha" type="password" size="small" error={error} helperText={error && 'Senha inválida'} fullWidth />
+            <TextField {...register('password')} label="Senha" type="password" size="small" error={!!errors.password} helperText={errors.password && errors.password.message} fullWidth />
           </Grid>
           <Grid item>
             <LoadingButton type="submit" variant="contained" loading={loading} fullWidth>ENTRAR</LoadingButton>
